58a77cb9b4822175dfc464a39fa19a3746bc9f55
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2024-10-09 23:11:53 +0200

    init driverAdapter & multischema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,32 +1,23 @@
+import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@prisma/client"
+import { Pool } from "pg"
 
 import { env } from "./env"
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    datasourceUrl: `${env.RDS_ENGINE}://${env.RDS_USERNAME}:${env.RDS_PASSWORD}@${env.RDS_HOST}:${env.RDS_PORT}/${env.RDS_DBNAME}`,
-    log:
-      env.NODE_ENV === "development" && env.RDS_DEBUG
-        ? [
-            {
-              emit: "event",
-              level: "query",
-            },
-            {
-              emit: "stdout",
-              level: "error",
-            },
-            {
-              emit: "stdout",
-              level: "info",
-            },
-            {
-              emit: "stdout",
-              level: "warn",
-            },
-          ]
-        : ["error"],
+const createPrismaClient = () => {
+  const connectionString = `${env.RDS_ENGINE}://${env.RDS_USERNAME}:${env.RDS_PASSWORD}@${env.RDS_HOST}:${env.RDS_PORT}/${env.RDS_DBNAME}`
+
+  const pool = new Pool({ connectionString })
+  const adapter = new PrismaPg(pool)
+
+  const client = new PrismaClient({
+    adapter,
   })
+
+  return client
+
+  //  return withPgAdapter(client)
+}
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined
